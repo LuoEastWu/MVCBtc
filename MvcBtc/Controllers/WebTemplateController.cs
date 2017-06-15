ae03a885067d0b7ae4bd48f68165c85ec123cb24
ldwql@hotmail.com <ldwql@hotmail.com>
2017-06-15 12:11:17 +0800

    22

---
+++ MvcBtc/Controllers/WebTemplateController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBtc.Controllers
+{
+    public class WebTemplateController : Controller
+    {
+        //
+        // GET: /WebTemplate/
+
+        public ActionResult Top()
+        {
+            return View();
+        }
+
+        public ActionResult MemberCenter_Top()
+        {
+            return View();
+        }
+
+        public ActionResult MemberCenter_Top()
+        {
+            return View();
+        }
+
+    }
+}
