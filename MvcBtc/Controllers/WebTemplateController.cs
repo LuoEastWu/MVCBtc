8ce116bf078f3f61da4d424310bf0cb31815fa98
ldwql@hotmail.com <ldwql@hotmail.com>
2017-06-17 02:30:47 +0800

    55

--- MvcBtc/Controllers/WebTemplateController.cs
+++ MvcBtc/Controllers/WebTemplateController.cs
@@ -10,38 +10,57 @@ namespace MvcBtc.Controllers
     {
         //
         // GET: /WebTemplate/
-    
-        public ActionResult Top()
+        public PartialViewResult Top()
+        {
+            JY.BLL.ProductType ptbll = new JY.BLL.ProductType();
+            ViewData["ProductTypeList"] = Collections.DataToList.ToList<JY.Model.ProductType>(ptbll.GetListByPage(" IsDelete=0 and fid=0", " sort ", 0, 10).Tables[0]);
+            JY.BLL.InformationType itbll = new JY.BLL.InformationType();
+            ViewData["InformationTypeList"] = Collections.DataToList.ToList<JY.Model.InformationType>(itbll.GetListByPage(" IsDelete=0 and fid=0", " sort ", 0, 10).Tables[0]);
+            return PartialView();
+        }
+        /// <summary>
+        /// 获取产品子类型
+        /// </summary>
+        public static List<JY.Model.ProductType> rptProductType(long typeId)
+        {
+            try
+            {
+                JY.BLL.ProductType bll = new JY.BLL.ProductType();
+                return Collections.DataToList.ToList<JY.Model.ProductType>(bll.GetListByPage(" IsDelete=0 and fid=" + typeId, " sort ", 0, 15).Tables[0]);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public PartialViewResult Left()
         {
             return PartialView();
         }
-        public ActionResult Left()
+
+
+        public PartialViewResult Foot()
         {
-            return View();
+            return PartialView();
+        }
+
+        public PartialViewResult MemberCenter_Top()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult MemberCenter_Left()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult MemberCenter_Foot()
+        {
+            return PartialView();
         }
 
 
-        public ActionResult Foot()
-        {
-            return View();
-        }
-
-        public ActionResult MemberCenter_Top()
-        {
-            return View();
-        }
-
-        public ActionResult MemberCenter_Left()
-        {
-            return View();
-        }
-
-        public ActionResult MemberCenter_Foot()
-        {
-            return View();
-        }
-
-     
 
 
     }
